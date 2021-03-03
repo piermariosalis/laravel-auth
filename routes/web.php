681578475ad0dad5940136163fe198b0231cbec4
piermariosalis <piermario.salis@aol.com>
2021-03-03 10:09:58 +0100

    make contact controller

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PageController@index')->name('homepage');
 
 Route::get('about', 'PageController@about')->name('about');
-Route::get('contacts', 'PageController@contacts')->name('contacts');
+// Route::get('contacts', 'PageController@contacts')->name('contacts');
+Route::get('contacts', 'ContactController@form')->name('contacts');
+
 
 Auth::routes(['register' => false]);
 
